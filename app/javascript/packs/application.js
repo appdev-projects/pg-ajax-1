84eebaa8e50ec93b9e9dd559929498bba998a293
Raghu Betina <raghu@firstdraft.com>
2021-05-06 09:59:49 -0500

    Remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,25 +4,25 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
-document.addEventListener('turbolinks:before-cache', () => {
+// document.addEventListener('turbolinks:before-cache', () => {
   // Manually tear down bootstrap modals before caching. If turbolinks
   // caches the modal then tries to restore it, it breaks bootstrap's JS.
   // We can't just use bootstrap's `modal('close')` method because it is async.
   // Turbolinks will cache the page before it finishes running.
-  if (document.body.classList.contains('modal-open')) {
-    $('.modal')
-      .hide()
-      .removeAttr('aria-modal')
-      .attr('aria-hidden', 'true');
-    $('.modal-backdrop').remove();
-    $('body').removeClass('modal-open');
-  }
-});
+  // if (document.body.classList.contains('modal-open')) {
+//     $('.modal')
+//       .hide()
+//       .removeAttr('aria-modal')
+//       .attr('aria-hidden', 'true');
+//     $('.modal-backdrop').remove();
+//     $('body').removeClass('modal-open');
+//   }
+// });
