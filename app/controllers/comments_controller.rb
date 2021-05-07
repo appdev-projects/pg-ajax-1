3deb0aa33d7dc3215956d866fdb4003055496219
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-05-07 00:35:50 +0000

    save my edits

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,7 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @comment = Comment.new
+    
   end
 
   # GET /comments/1/edit
@@ -33,6 +34,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js { render template "comments/create.js.erb" }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -59,6 +61,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+      format.js { render template: "comments/destroy.js.erb" }
     end
   end
 
