b7000071ebd07bc4a068134bea446fcffc772545
johnson-cole <colejohnson@chicagobooth.edu>
2021-05-07 00:33:54 +0000

    Added create, edit, update AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,7 @@ class CommentsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
+      format.js {render template: "comments/edit.js.erb"}
     end
   end
 
@@ -47,6 +48,7 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
+        format.js {render template: "comments/update.js.erb"}
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
