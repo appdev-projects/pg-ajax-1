a67addc9576b38927beef227f11057e4b054bccb
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-05-30 22:38:50 +0000

    create and delete  with jQuery and Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -59,6 +60,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+      format.js {render template: "comments/destroy.js.erb"}
     end
   end
 
